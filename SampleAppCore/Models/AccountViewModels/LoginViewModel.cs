c3181564d5e8be5f60943b991402a15a94e7e2a8
HungTA <khoatran121294>
2018-02-16 15:40:45 +0700

    add login page + logout

--- SampleAppCore/Models/AccountViewModels/LoginViewModel.cs
+++ SampleAppCore/Models/AccountViewModels/LoginViewModel.cs
@@ -9,7 +9,7 @@ namespace SampleAppCore.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        public string UserName { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
