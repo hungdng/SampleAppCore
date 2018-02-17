b7ec4bb54ca5e9e34d6769c69d5a05222b7fcef3
HungTA <khoatran121294>
2018-02-17 12:42:57 +0700

    reload ajax with view component

---
+++ SampleAppCore/Controllers/AjaxContentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAppCore.Controllers
+{
+    public class AjaxContentController : Controller
+    {
+        public IActionResult HeaderCart()
+        {
+            return ViewComponent("HeaderCart");
+        }
+    }
+}
