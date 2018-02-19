a12c8c7a46c30277b12c765547f3eaf5ccf8da25
HungTA <khoatran121294>
2018-02-19 22:35:14 +0700

    add project api

---
+++ SampleAppCore.WebApi/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using SampleAppCore.Data.EF;
+using SampleAppCore.Data.EF.Repositories;
+using SampleAppCore.Data.IRepositories;
+using SampleAppCore.Infrastructure.Interfaces;
+using SampleAppCore.Service.Implementation;
+using SampleAppCore.Service.Interfaces;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SampleAppCore.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                   options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                       b => b.MigrationsAssembly("SampleAppCore.Data.EF")));
+
+            services.AddCors(o => o.AddPolicy("SampleCoreCorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+            services.AddAutoMapper();
+
+
+            services.AddSingleton(Mapper.Configuration);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<AutoMapper.IConfigurationProvider>(), sp.GetService));
+            services.AddTransient(typeof(IUnitOfWork), typeof(EFUnitOfWork));
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+            services.AddTransient<IProductCategoryService, ProductCategoryService>();
+
+            services.AddMvc().
+                AddJsonOptions(options =>
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "SampleCore Project",
+                    Description = "SampleCore API Swagger surface",
+                    Contact = new Contact { Name = "HungTA", Email = "hungdng.92@gmail.com", Url = "http://cameraquangduc.com" },
+                    License = new License { Name = "MIT", Url = "https://github.com/hungdng" }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseCors("SampleCoreCorsPolicy");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Project API v1.1");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
